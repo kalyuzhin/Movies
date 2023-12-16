591d7a8f8ea914d4342648111dd0e90b15648c5d
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2023-12-16 20:25:16 +0300

    Implement consts and request to Notion API
    Sketch of future functions

--- notion/notion.py
+++ notion/notion.py
@@ -4,4 +4,42 @@ import requests
 
 with open('../config.txt') as config:
     TOKEN = config.readline().strip()
+    MOVIE_PAGE = config.readline().strip()
 
+HEADERS = {
+    'Authorization': f'Bearer {TOKEN}',
+    'Notion-Version': '2022-06-28',
+    'Content-Type': 'application/json',
+
+}
+
+DATA = {
+    'children': [
+        {
+            'object': 'block',
+            'type': 'bookmark',
+            'bookmark': {
+                'caption': [{"type": "text", "text": {"content": f"Американский психопат"}}],
+                'url': f'https://www.kinopoisk.ru/film/588'
+
+            }
+        }
+    ]
+}
+
+BASE_URL = 'https://api.notion.com/v1'
+
+request = requests.get(BASE_URL + f"/blocks/{MOVIE_PAGE}/children", headers=HEADERS).json()
+
+while request['results'][0]['type'] != 'column':
+    request = requests.get(BASE_URL + f"/blocks/{request['results'][0]['id']}/children", headers=HEADERS).json()
+
+try:
+    request = requests.patch(
+        BASE_URL + f"/blocks/{request['results'][1]['id']}/children", headers=HEADERS, data=json.dumps(DATA))
+    if request.status_code == 200:
+        print('Done...')
+    else:
+        print(request.text)
+except Exception as ex:
+    print('Error!!!\n', ex)
