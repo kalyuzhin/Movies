b0bf2825e20f4a272934a2a4e757b0c90d784b0a
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2023-12-16 20:10:09 +0300

    Implement imdb parsing function

--- tools/imdb_functions.py
+++ tools/imdb_functions.py
@@ -8,3 +8,12 @@ def create_imdb_url(name: str) -> str:
     return f'https://www.imdb.com/find/?q={name}&ref_=nv_sr_sm'
 
 
+def parse_imdb_page(url: str) -> dict:
+    result = dict()
+    request = requests.get(url).text
+    soup = BeautifulSoup(request)
+    for elem in soup.find_all('div', class_='ipc-metadata-list-summary-item__tc'):
+        # elem.find_next("a", class_="ipc-metadata-list-summary-item__t").attrs["href"]
+        elem = elem.find_next('a', class_='ipc-metadata-list-summary-item__t')
+        result[f'{elem.text} ({elem.find_next("span").text})'] = f"https://www.imdb.com/{elem.attrs['href']}"
+    return result
