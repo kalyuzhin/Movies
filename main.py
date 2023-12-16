055f9bd700ff4fbbd88958107b2ec363e240a260
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2023-12-16 20:50:39 +0300

    Add final logic in main
    Implement change data function in notion.py
    little fixes in already_watched and want_to_watch functions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from notion import *
 
 
 def main() -> None:
-    name = input("Введите название фильма:\n")
+    name = input("Введите название фильма или сериала:\n")
     service = input("Введите, где хотите найти его(кинопоиск или imdb):\n")
     if service.strip().lower() == 'кинопоиск':
         result = kinopoisk_functions.parse_kinopoisk_page(kinopoisk_functions.create_kinopoisk_url(name))
@@ -16,6 +16,14 @@ def main() -> None:
     for elem in result:
         print(f'{i}.{elem} - {result[elem]}')
         i += 1
+    print("Выберите нужный вам элемент(скопируйте и вставьте его название):\n")
+    name = input()
+    print("Вы хотите его посмотреть или вы уже его просмотрели?:\n")
+    choice = input()
+    if choice.strip().lower() == 'хочу посмотреть':
+        notion.want_to_watch(notion.change_data(result, name))
+    elif choice.strip().lower() == 'просмотрен':
+        notion.already_watched(notion.change_data(result, name))
 
 
 if __name__ == '__main__':
--- notion/notion.py
+++ notion/notion.py
@@ -14,19 +14,19 @@ HEADERS = {
     'Content-Type': 'application/json',
 }
 
-DATA = {
-    'children': [
-        {
-            'object': 'block',
-            'type': 'bookmark',
-            'bookmark': {
-                'caption': [{"type": "text", "text": {"content": f"Американский психопат"}}],
-                'url': f'https://www.kinopoisk.ru/film/588'
-
-            }
-        }
-    ]
-}
+# DATA = {
+#     'children': [
+#         {
+#             'object': 'block',
+#             'type': 'bookmark',
+#             'bookmark': {
+#                 'caption': [{"type": "text", "text": {"content": f"Американский психопат"}}],
+#                 'url': f'https://www.kinopoisk.ru/film/588'
+#
+#             }
+#         }
+#     ]
+# }
 
 BASE_URL = 'https://api.notion.com/v1'
 
@@ -47,13 +47,13 @@ BASE_URL = 'https://api.notion.com/v1'
 #     print('Error!!!\n', ex)
 
 
-def already_watched() -> None:
+def already_watched(data: dict) -> None:
     try:
         request = requests.get(BASE_URL + f"/blocks/{MOVIE_PAGE}/children", headers=HEADERS).json()
         while request['results'][0]['type'] != 'column':
             request = requests.get(BASE_URL + f"/blocks/{request['results'][0]['id']}/children", headers=HEADERS).json()
         request = requests.patch(
-            BASE_URL + f"/blocks/{request['results'][1]['id']}/children", headers=HEADERS, data=json.dumps(DATA))
+            BASE_URL + f"/blocks/{request['results'][1]['id']}/children", headers=HEADERS, data=json.dumps(data))
         if request.status_code == 200:
             print('Done...')
         else:
@@ -62,16 +62,32 @@ def already_watched() -> None:
         print('Ошибка!!!\n', ex)
 
 
-def want_to_watch() -> None:
+def want_to_watch(data: dict) -> None:
     try:
         request = requests.get(BASE_URL + f"/blocks/{MOVIE_PAGE}/children", headers=HEADERS).json()
         while request['results'][0]['type'] != 'column':
             request = requests.get(BASE_URL + f"/blocks/{request['results'][0]['id']}/children", headers=HEADERS).json()
         request = requests.patch(
-            BASE_URL + f"/blocks/{request['results'][0]['id']}/children", headers=HEADERS, data=json.dumps(DATA))
+            BASE_URL + f"/blocks/{request['results'][0]['id']}/children", headers=HEADERS, data=json.dumps(data))
         if request.status_code == 200:
             print('Done...')
         else:
             print(request.text)
     except Exception as ex:
         print('Ошибка!!!\n', ex)
+
+
+def change_data(dic: dict, name: str) -> dict:
+    data = {
+        'children': [
+            {
+                'object': 'block',
+                'type': 'bookmark',
+                'bookmark': {
+                    'caption': [{"type": "text", "text": {"content": f"{name}"}}],
+                    'url': f'{dic[name]}'
+                }
+            }
+        ]
+    }
+    return data
