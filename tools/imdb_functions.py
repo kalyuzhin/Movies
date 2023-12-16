a57361d81096c5a65bd21535316ad895cc8f1457
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2023-12-16 20:10:56 +0300

    Fix function

--- tools/imdb_functions.py
+++ tools/imdb_functions.py
@@ -2,6 +2,10 @@ import requests
 
 from bs4 import BeautifulSoup
 
+USER_AGENT = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1.2 Safari/605.1.15'
+}
+
 
 def create_imdb_url(name: str) -> str:
     name = name.strip().replace(' ', '%20')
@@ -10,7 +14,7 @@ def create_imdb_url(name: str) -> str:
 
 def parse_imdb_page(url: str) -> dict:
     result = dict()
-    request = requests.get(url).text
+    request = requests.get(url, headers=USER_AGENT).text
     soup = BeautifulSoup(request)
     for elem in soup.find_all('div', class_='ipc-metadata-list-summary-item__tc'):
         # elem.find_next("a", class_="ipc-metadata-list-summary-item__t").attrs["href"]
